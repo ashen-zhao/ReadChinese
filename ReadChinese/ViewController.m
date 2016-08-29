23b094875a69cfc9340a47842628a1437bbe6f47
Eric Wang <Eric@Erics-MacBook-Pro-2.local>
2016-08-29 17:28:39 +0800

    添加对storyboard 及 xib文件中的中文解析

--- ReadChinese/ViewController.m
+++ ReadChinese/ViewController.m
@@ -16,6 +16,7 @@
 @property (weak) IBOutlet NSScrollView *txtShowChinese;
 @property (weak) IBOutlet NSButton *deleteInOneFile;
 @property (weak) IBOutlet NSButton *deleteInAllFiles;
+@property (weak) IBOutlet NSButton *includeXMLFiles;
 @property (weak) IBOutlet NSButton *tradition;
 
 @property (nonatomic, strong)  NSTextView *txtView;
@@ -42,6 +43,11 @@
 - (IBAction)deleteInOneFile:(NSButton *)sender {
     self.deleteInAllFiles.state = 0;
 }
+
+- (IBAction)includeXMLFiles:(NSButton *)sender {
+    self.includeXMLFiles.state = 0;
+}
+
 
 
 - (IBAction)OpenFile:(NSButton *)sender {
@@ -83,19 +89,22 @@
     
     NSDirectoryEnumerator *direnum = [manager enumeratorAtPath:home];
     NSMutableArray *files = [NSMutableArray arrayWithCapacity:42];
+    NSMutableArray *xmlFiles = [NSMutableArray arrayWithCapacity:42];
     
     NSString *filename ;
-    NSArray *extension = @[@"m", @"h"];
+    NSArray *extension = @[];
+    NSArray *xmlExtension = @[@"storyboard", @"xib"];
     while (filename = [direnum nextObject]) {
-        for (NSString *ext in extension) {
-            if ([[filename pathExtension] isEqualToString:ext]) {
-                [files addObject: filename];
-            }
+        if ([extension containsObject:filename.pathExtension]) {
+            [files addObject: filename];
+        }
+        else if([xmlExtension containsObject:filename.pathExtension]){
+            [xmlFiles addObject:filename];
         }
     }
-    NSEnumerator *fileenum;
-    fileenum = [files objectEnumerator];
+    NSEnumerator *fileenum = [files objectEnumerator];
     NSInteger chineseCount = 0;
+    
     while (filename = [fileenum nextObject]) {
         
         NSMutableArray *dataInOneFile = [NSMutableArray array];
@@ -143,6 +152,61 @@
         }
     }
     
+    if (self.includeXMLFiles.state) {//处理 storyboard 及 xib文件
+        fileenum = [xmlFiles objectEnumerator];
+        while (filename = [fileenum nextObject]) {
+            
+            NSMutableArray *dataInOneFile = [NSMutableArray array];
+            
+            NSString *str=[NSString stringWithContentsOfFile:[NSString stringWithFormat:@"%@/%@", home, filename] encoding:NSUTF8StringEncoding error:nil];
+            
+            NSXMLDocument *doc = [[NSXMLDocument alloc] initWithXMLString:str options:kNilOptions error:nil];
+            if (!doc) {
+                continue;
+            }
+            
+            NSArray *nodes = [doc nodesForXPath:@"//label[@text] | //button/state[@title] | //navigationItem[@title] | //barButtonItem[@title]" error:nil];
+            
+            NSString *newFileName =  [NSString stringWithFormat:@"\n/*\n%@\n*/", [[filename componentsSeparatedByString:@"/"] lastObject]];
+            BOOL isHasFileName = NO;
+            BOOL isHasChineseInFile = NO;
+            for (NSXMLElement *element in nodes) {
+                if (!isHasFileName) {
+                    [dataMSet addObject:newFileName];
+                }
+                
+                //label和文本属性名为text
+                NSString *atrributeName = [element.name isEqualToString:@"label"] ? @"text" : @"title";
+                
+                //获取属性元素
+                NSXMLNode *node = [element attributeForName:atrributeName];
+                
+                NSString *mStr = node.stringValue ?: @"";
+                mStr = [NSString stringWithFormat:@"\"%@\"", mStr];
+                isHasFileName = YES;
+                if (self.deleteInOneFile.state) {
+                    if ([dataInOneFile containsObject:mStr]){//除去本文件中重复出现的字符串
+                        continue;
+                    }
+                    [dataInOneFile addObject:mStr];
+                }
+                
+                if (self.deleteInAllFiles.state) {
+                    if ([dataMSet containsObject:mStr]){//除去所有文件中重复出现的字符串
+                        continue;
+                    }
+                }
+                chineseCount++;
+                [dataMSet addObject:mStr];
+                isHasChineseInFile = YES;
+            }
+            if (!isHasChineseInFile) {
+                [dataMSet removeObject:newFileName];
+            }
+        }
+    }
+    
+    
     for (NSString *txt in dataMSet) {
         if ([txt containsString:@"/*"] && [txt containsString:@"*/"]) {
             [dataMstr appendString:txt];
@@ -153,6 +217,7 @@
         [dataMstr appendString:@";"];
         [dataMstr appendString:@"\n"];
     }
+    
     [dataMstr writeToFile:self.txtShowOutPath.placeholderString atomically:YES encoding:NSUTF8StringEncoding error:nil];
     
     NSMutableString *finalStr = [NSMutableString stringWithFormat:@"\n共有 %ld 个中文字符串\n", chineseCount];
